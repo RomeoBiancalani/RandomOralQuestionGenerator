42373d688192ca285276db9350311e94c7bb3d00
giulia.aloia05@gmail.com <giulia@pop-os.localdomain>
2023-01-20 18:42:27 +0100

    working on singleclassView UI

--- src/views/SingleclassView.vue
+++ src/views/SingleclassView.vue
@@ -4,87 +4,106 @@
     <div class="row"> 
       <div class="col">
         <div class="container-list">
-                <div class="list-group notTestedList">
-                    <li class="list-group-item list-group-item-primary">Lista Studenti</li>
-                    <li
-                      class="list-group-item" 
-                      v-for="(student, index) in studentsList" 
-                      :key="index"
-                    >
-                    <span class="nomeStudente">
-                      {{index+1}}) {{student}}
+            <div class="list-group">
+                <li class="list-group-item list-group-item-primary">Lista Studenti</li>
+                <li
+                  class="list-group-item" 
+                  v-for="(student, index) in studentsList" 
+                  :key="index"
+                >
+                <span class="nomeStudente">
+                  {{index+1}}) {{student}}
+                </span>
+                    <span class="float-end">
+                      <span class="icon-action" @click="editStudent(index)">
+                        <font-awesome-icon icon="fa-solid fa-user-pen" />
+                      </span>
+                      <span>
+                        <font-awesome-icon icon="fa-solid fa-trash" />
+                      </span>
                     </span>
-                        <span class="float-end">
-                          <span class="icon-action" @click="editStudent(index)">
-                            <font-awesome-icon icon="fa-solid fa-user-pen" />
-                          </span>
-                          <span>
-                            <font-awesome-icon icon="fa-solid fa-trash" />
-                          </span>
-                        </span>
-                    </li>
-                </div>
+                </li>
             </div>
+        </div>
       </div>
+
       <div class="col-6">
         <div class="container-callbuttons">
-                <div class="d-grid gap-3 col-30 ">
-                    <button @click="getRandomStudent()" class="btn btn-primary btn-lg" type="button" >Chiama uno studente random</button>
-                    <button @click="selectStudent()" class="btn btn-info btn-lg" type="button">Seleziona uno studente da chiamare</button>
-                </div>
+            <div class="d-grid gap-3 col-30 ">
+                <button @click="getRandomStudent()" class="btn btn-primary btn-lg" type="button" >Chiama uno studente random</button>
+                <button @click="selectStudent()" class="btn btn-info btn-lg" type="button">Seleziona uno studente da chiamare</button>
             </div>
-            <div class="d-flex" v-if = "showRandomCard" >
-                <div class="container-randomnumber justify-content-end">
-                    <div class="card text-bg-light border-primary mb-3" style="width: 25rem;height: 20rem;">
-                        <div class="card-body">
-                            <br>
-                            <!-- Display titolo -->
-                            <h3 class="card-title text-primary"><u><strong>Studente estratto:</strong></u></h3>
-                            <br>
-                            
-                            <!-- Display num random -->
-                            <div class="card border-primary mb-3 mx-auto" style="width: 10rem; height: 10rem;">
-                                <div class="card-body d-flex justify-content-center">
-                                    <br>
-                                    <div class="card-text text-center" v-if = "showRandomNumber" style="font-size: 36px;">
-                                      {{ randomNumber + 1}}
-                                    </div>
+        </div>
+        <div class="container-callbuttons" v-if = "showCalledMessage">
+          <div class="alert alert-success d-flex "  role="alert" style="margin-top: 200px; width: 25rem">
+             
+            <span><font-awesome-icon icon="fa-solid fa-circle-check" /></span>  
+            <div class="mx-auto" style="font-size:20px">Studente chiamato!</div>   
+          </div>
+        </div>
+        
+
+        
+                
+        <div class="d-flex" v-if = "showRandomCard" >
+            <div class="container-randomnumber justify-content-end">
+                <div class="card text-bg-light border-primary mb-3" style="width: 25rem;height: 20rem;">
+                    <div class="card-body">
+                        <br>
+                        <!-- Display titolo -->
+                        <h3 class="card-title text-primary"><u><strong>Studente estratto:</strong></u></h3>
+                        <br>
+                        
+                        <!-- Display num random -->
+                        <div class="card border-primary mb-3 mx-auto" style="width: 10rem; height: 10rem;">
+                            <div class="card-body d-flex justify-content-center">
+                                <br>
+                                <div class="card-text text-center" v-if = "showRandomNumber" style="font-size: 36px;">
+                                  {{ randomNumber + 1}}
                                 </div>
                             </div>
-                            {{ calledStudent }}
-                            
                         </div>
+                        
                     </div>
                 </div>
-                <div class="container-testchoicebuttons">
-                    <br><br>
-                    <div class="d-grid gap-3 col-20">
-                        <button @click="addToTested()" class="btn btn-success btn-lg" type="button">Interroga oggi</button>
-                        <button @click="this.showRandomNumber = false" class="btn btn-info btn-lg" type="button">Non chiamare per oggi</button> <!-- clear card random number -->
-                        <button @click="this.showRandomCard = false" class="btn btn-danger btn-lg" type="button">Esci</button>
-                    </div>
+                <div class="alert alert-primary" role="alert" style="margin-top: 50px">
+                  <h4><u>Nome studente:</u></h4><br><h5>{{ calledStudent }}</h5>
                 </div>
-            </div>  
-      </div>
-      <div class="col">
-        <div class="container-notTested" v-if="showNotTestedList">
-                <div class="list-group notTestedList justify-content-end">
-                    <button type="button" class="list-group-item list-group-item-action disabled" aria-disabled="true" 
-                    style="background-color: #FDD231; text-align: center">
-                        <strong>Studenti non interrogati</strong>
-                    </button>
-                    <button @click="showCalledMessage = true, addToTested()" type="button" class="list-group-item list-group-item-action" v-for="student in alreadyTested" :key="student">
-                        {{ studentsList[student]}}
-                    </button>
-                </div>
-                <div class="testedMessage" v-if = "showCalledMessage">
-                    <div class="card text-bg-info mb-3" style="max-width: 18rem;">
-                        <div class="card-body">
-                            Studente chiamato!
-                        </div>
-                    </div>
+              
+            </div>
+           
+            <div class="container-testchoicebuttons">
+                <br><br>
+                <div class="d-grid gap-3 col-20">
+                    <button @click="this.showCalledMessageRandom = true; addToTested()" class="btn btn-success btn-lg" type="button">Interroga oggi</button>
+                    <button @click="this.showRandomNumber = false; getRandomStudent()" class="btn btn-info btn-lg" type="button">Non chiamare per oggi</button> <!-- clear card random number -->
+                    <button @click="this.showRandomCard = false; this.showCalledMessageRandom = false" class="btn btn-danger btn-lg" type="button">Esci</button>
                 </div>
             </div>
+        </div>  
+       
+       
+               
+      </div>
+      <div class="col">
+        <div class="container-testchoicebuttons">
+          <br><br>
+          <div class="alert alert-success d-flex"   v-if = "showCalledMessageRandom" role="alert"> 
+            <span><font-awesome-icon icon="fa-solid fa-circle-check" /></span>  
+            <div class="mx-auto" style="font-size:20px">Studente chiamato!</div>   
+          </div>
+        </div>
+    
+        <div class="container-list">
+          <div class="list-group notTestedList" v-if="showNotTestedList">
+            <div class="list-group-item list-group-item-info justidy-content-end">Lista studenti non interrogati</div>
+            <button @click="showCalledMessage = true, addToTested()" type="button" class="list-group-item list-group-item-action" v-for="index in alreadyTested" :key="index">
+              <span class="nomeStudente">
+                {{ index+1 }}) {{ studentsList[index] }}
+              </span>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -146,15 +165,18 @@ export default {
       randomNumber: 0,
       studentsList: ["stud 1", "stud 2","stud 3"],
       studentsNumber: 0,
-      alreadyTested: [0],
+      alreadyTested: [],
       showRandomCard: false, //visualizza card num random
       showRandomNumber: true, //visualizza num random
       showNotTestedList: false, //visualizza lista studenti non interrogati
       showCalledMessage: false, //visualizza il messaggio di chiamata
+      showCalledMessageRandom: false, //viene visualizzato quando viene interrogato lo studente random
+      showEverybodyTestedAlert: false,
       nomeStudente: "",
       indexStudente: 0,
       studentNameInput: "",
       calledStudent: "", //visualizza nome studente chiamato
+      
     };
   },
   //TODO: correggere il ricavo dati dal db
@@ -193,14 +215,19 @@ export default {
       this.showRandomCard = true;
       this.showRandomNumber = true;
       this.showNotTestedList = false;
+      this.showCalledMessage = false;
+      this.showCalledMessageRandom = false;
+      
     
       if (this.alreadyTested.length != this.studentsList.length) {
         // se non sono stati interrogati tutti
         do {
           index = Math.floor(Math.random() * (this.studentsList.length-1)); // random index
         } while(this.alreadyTested.includes(index)); // ripete se trova uno studente che era gia' stato interrogato
-     } else {
-       alert("Tutti gli studenti di questa classe sono stati interrogati!"); //TODO: sistema alert
+      } else {
+        alert("Tutti gli studenti di questa classe sono stati interrogati!"); //TODO: sistema alert
+        this.showEverybodyTestedAlert = true;
+        
       }
       // estrai studente
       if (index != -1) {
@@ -215,12 +242,16 @@ export default {
       //aggiunge uno studente alla lista di chi e' gia' stato interrogato
       alreadyTested.push(index); //update
       localforage.setItem("questionedStudents", this.alreadyTested);
+      //show messaggio
+      
     },
     //funzioni per selezionare uno studente specifico da interrogare
     selectStudent() {
       //se si sceglie di non chiamare a caso
       this.showRandomCard = false; //non far vedere la card per generare random
       this.showNotTestedList = true; //display lista persone da interrogare
+      this.showCalledMessageRandom = false;
+      
     },
 
   },
@@ -231,9 +262,7 @@ export default {
 .row {
   margin-top: 20px;
 }
-// .container {
-//   padding: 25px;
-// }
+
 .d-flex {
   align-items: center; // Centra in altezza
 }
@@ -257,6 +286,11 @@ export default {
   }
 }
 
+
+
+
+
+
 .card-header {
   text-align: center;
   font-size: 25px;
@@ -271,6 +305,7 @@ export default {
   //contiene card num random
 
   padding-top: 50px;
+  margin-left:200px;
 }
 
 .container-testchoicebuttons {
@@ -283,16 +318,10 @@ export default {
   //contiene opzioni se chiamare random o no
   align-items: center;
   display: flex;
+  margin-left:200px;
 }
 
-.container-notTested {
-  //contiene lista studenti non ancora interrogati
-  font-size: 20px;
-  text-align: left;
-  padding-top: 25px;
-  display: flex;
-  align-items: center;
-}
+
 
 .testedMessage {
   padding-left: 100px;
